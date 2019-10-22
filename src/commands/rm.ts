a5644d9392866ed60dfe47ed56561cfc2ae976b1
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-22 00:37:37 -0400

    fix: improved "indo rm"
    Avoid removing a directory from ".indo.json" if the user answered false to the deletion prompt

--- src/commands/rm.ts
+++ src/commands/rm.ts
@@ -7,25 +7,43 @@ import { confirm } from '../core/helpers'
 
 export default async function(cfg: RootConfig) {
   let changed = false
-  for (let path of slurm()) {
-    path = relative(cfg.root, resolve(path))
-    if (cfg.repos[path]) {
-      delete cfg.repos[path]
-      changed = true
-    } else {
-      const len = cfg.vendor.length
-      cfg.vendor = cfg.vendor.filter(glob => {
-        return glob !== path && !glob.startsWith(path + '/')
-      })
-      if (len > cfg.vendor.length) {
+  for (let root of slurm()) {
+    root = resolve(root)
+
+    const rootDir = relative(cfg.root, root)
+    if (!fs.isDir(rootDir)) {
+      log.error(`Expected ${log.lpink(rootDir)} to be a directory`)
+      continue
+    }
+
+    // Confirm deletion
+    const ok = await confirm(`Delete ${log.yellow('./' + root)} forever?`)
+    if (!ok) continue
+
+    // Delete from disk
+    fs.remove(root, true)
+
+    // Delete descendants from "repos"
+    for (const repoPath in cfg.repos) {
+      if (isDescendant(repoPath, root)) {
+        delete cfg.repos[repoPath]
         changed = true
       }
     }
-    if (await confirm(`Delete ${log.yellow('./' + path)} forever?`)) {
-      fs.remove(path, true)
+
+    // Delete descendants from "vendor"
+    const len = cfg.vendor.length
+    cfg.vendor = cfg.vendor.filter(glob => isDescendant(glob, root))
+    if (len > cfg.vendor.length) {
+      changed = true
     }
   }
   if (changed) {
     saveConfig(cfg)
   }
+}
+
+/** Returns true if `parent` is equal to (or a parent of) the `path` argument */
+function isDescendant(path: string, parent: string) {
+  return path === parent || path.startsWith(parent + '/')
 }
