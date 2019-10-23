898d39fd0b009846ce489a1652ca3603443d6ff4
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-23 10:42:22 -0400

    nit: add confirmation logs to "indo init"

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,7 +1,15 @@
+import log from 'lodge'
 import fs from 'saxon/sync'
 import { loadConfig } from '../core/config'
 
 export default () => {
   fs.write('.indo.json', '{}')
+  log(
+    log.green('✓'),
+    'Created',
+    log.lgreen('.indo.json'),
+    'in',
+    log.gray(process.cwd())
+  )
   return loadConfig()
 }
