9ab69a250baabb383d4514e6c5d1a98cadccb3d3
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-22 20:59:17 -0400

    docs: custom syntax for help formatting

--- src/cli.ts
+++ src/cli.ts
@@ -30,7 +30,36 @@ if (helpArg) {
     basename(cmdPath).replace(/\.js$/, '.md')
   )
   if (fs.exists(helpPath)) {
-    log(fs.read(helpPath))
+    let help = ''
+
+    const rawHelp = fs.read(helpPath)
+    const tagRE = /<((?:\/[ ]*)?)([\w]+)>|([`])|([*])/g
+    const opened: { [key: string]: number } = {}
+    let lastIndex = 0
+    let match
+    while ((match = tagRE.exec(rawHelp))) {
+      let [, close, name, lcyan, bold] = match
+      if (lcyan || bold) {
+        name = lcyan ? 'lcyan' : 'bold'
+        close = opened[name] ? '/' : ''
+      }
+      if (close) {
+        const index = opened[name]
+        if (index !== void 0) {
+          delete opened[name]
+          help += log[name](rawHelp.slice(index, match.index))
+          lastIndex = match.index + match[0].length
+        }
+      } else {
+        help += rawHelp.slice(lastIndex, match.index)
+        opened[name] = lastIndex = match.index + match[0].length
+      }
+    }
+    help += rawHelp.slice(lastIndex)
+    for (const name in opened) {
+      fatal('Forgot to close', log.lcyan(`<${name}>`))
+    }
+    log(help)
   }
   process.exit()
 }
