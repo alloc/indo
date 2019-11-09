f5ed163056388b4a9007441e2d857b933913f277
aleclarson <alec.stanford.larson@gmail.com>
2019-11-09 00:09:41 -0500

    wip: append to gitignore

--- src/core/gitignore.ts
+++ src/core/gitignore.ts
@@ -5,7 +5,7 @@ import { isHomeDir } from './helpers'
 
 const readLines = (path: string) => fs.read(path).split(/\r?\n/)
 
-type GlobTree = { [pathId: string]: GlobMatcher | false }
+type GlobTree = { [pathId: string]: GlobMatcher | false | null }
 
 export class GitIgnore {
   readonly matchRootGlobs: GlobMatcher | null
@@ -20,6 +20,16 @@ export class GitIgnore {
     this.matchRootGlobs = createMatcher(rootGlobs)
   }
 
+  append(glob: string, rootDir = this.rootDir) {
+    if (!this._check(rootDir, glob)) {
+      const gitIgnorePath = join(rootDir, '.gitignore')
+      if (fs.isFile(gitIgnorePath)) {
+        const contents = fs.read(gitIgnorePath).trim() + '\n' + glob + '\n'
+        fs.write(gitIgnorePath, contents)
+      }
+    }
+  }
+
   test(file: string, name?: string) {
     if (!isAbsolute(file)) {
       throw Error('Expected an absolute path')
@@ -32,30 +42,39 @@ export class GitIgnore {
       if (match(file, name)) return true
     }
     for (let dir = dirname(file); !isHomeDir(dir); dir = dirname(dir)) {
-      const path = join(dir, '.gitignore')
-      const pathId = relative(this.rootDir, path)
-
-      match = this.globTree[pathId]
-      if (match !== false) {
-        if (match) {
-          if (match(file, name)) {
-            return true
-          }
-        } else if (fs.isFile(path)) {
-          const lines = readLines(path).filter(line => line && line[0] !== '#')
-          match = createMatcher(lines, glob => join(dir, glob))
-          this.globTree[pathId] = match || false
-          if (match && match(file, name)) {
-            return true
-          }
-        } else {
-          this.globTree[pathId] = false
-        }
+      if (this._check(dir, file, name)) {
+        return true
       }
 
       // Never use .gitignore outside the git repository.
       if (fs.isDir(join(dir, '.git'))) {
         break
+      }
+    }
+    return false
+  }
+
+  _check(rootDir: string, file: string, name?: string) {
+    const gitIgnorePath = join(rootDir, '.gitignore')
+    const gitIgnoreId = relative(this.rootDir, gitIgnorePath)
+
+    let match = this.globTree[gitIgnoreId]
+    if (match !== false) {
+      if (match) {
+        if (match(file, name)) {
+          return true
+        }
+      } else if (fs.isFile(gitIgnorePath)) {
+        const lines = readLines(gitIgnorePath).filter(
+          line => line && line[0] !== '#'
+        )
+        match = createMatcher(lines, glob => join(rootDir, glob))
+        this.globTree[gitIgnoreId] = match || false
+        if (match && match(file, name)) {
+          return true
+        }
+      } else {
+        this.globTree[gitIgnoreId] = false
       }
     }
     return false
