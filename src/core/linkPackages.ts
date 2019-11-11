bc0f8689c46ea777d23852a5e06d3d19698191a8
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-11 09:01:13 -0500

    feat: add spinner and success message to "linkPackages" function

--- src/core/linkPackages.ts
+++ src/core/linkPackages.ts
@@ -2,7 +2,7 @@ import { dirname, join, relative } from 'path'
 import semver from 'semver'
 import { RootConfig } from './config'
 import { fs } from './fs'
-import { isPathEqual, log, splitNameVersion } from './helpers'
+import { isPathEqual, log, spin, splitNameVersion } from './helpers'
 import { loadPackages } from './loadPackages'
 import { loadVendors } from './loadVendors'
 import { StringMap } from './Package'
@@ -13,6 +13,7 @@ export function linkPackages(
   opts: { force?: boolean } = {}
 ) {
   console.time('link packages')
+  const spinner = spin('Linking packages...')
 
   const vendor = loadVendors(cfg)
   for (const pkg of Object.values(packages)) {
@@ -28,6 +29,7 @@ export function linkPackages(
     const nodeModulesPath = join(pkg.root, 'node_modules')
     for (let [alias, version] of Object.entries(deps)) {
       if (/^(link|file):/.test(version)) continue
+      spinner.start()
 
       let name = alias
       if (version.startsWith('npm:')) {
@@ -40,6 +42,7 @@ export function linkPackages(
       const dep = packages[name] || vendor[name]
       if (dep) {
         if (version && !semver.satisfies(dep.version, version)) {
+          spinner.stop()
           log.warn(
             'Local package',
             log.lgreen('./' + relative(cfg.root, dep.root)),
@@ -57,6 +60,7 @@ export function linkPackages(
           fs.remove(link, true)
           fs.mkdir(dirname(link))
           fs.link(link, target)
+          spinner.stop()
           log(
             log.green('+'),
             'Linked',
@@ -74,6 +78,7 @@ export function linkPackages(
               fs.remove(link)
               fs.mkdir(dirname(link))
               fs.link(link, target)
+              spinner.stop()
               log(
                 log.green('+'),
                 'Linked',
@@ -95,5 +100,7 @@ export function linkPackages(
     }
   }
 
+  spinner.stop()
+  log(log.green('✓'), 'Local packages are linked!')
   console.timeEnd('link packages')
 }
