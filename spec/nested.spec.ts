0d94a509aabf4cce7b73fd646de40914f64cd391
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-13 12:46:11 -0400

    test: 30s timeout for nested indo.json
    and snapshot the logs

--- spec/nested.spec.ts
+++ spec/nested.spec.ts
@@ -1,7 +1,25 @@
-test('nested indo', async () => {
-  process.chdir('nested')
-  await indo()
+test(
+  'nested indo',
+  async () => {
+    process.chdir('nested')
+    await indo()
 
-  expect(fs.isDir('gist')).toBeTruthy()
-  expect(fs.isDir('gist/mixpa')).toBeTruthy()
-})
+    expect(fs.isDir('gist')).toBeTruthy()
+    expect(fs.isDir('gist/node_modules')).toBeTruthy()
+
+    expect(fs.isDir('gist/mixpa')).toBeTruthy()
+    expect(fs.isDir('gist/mixpa/node_modules')).toBeTruthy()
+
+    expect(logs).toMatchInlineSnapshot(`
+      Array [
+        "+ Cloned gist from gist.github.com/aleclarson/3877431fe02171c150c401ecb9a5030e",
+        "+ Cloned gist/mixpa from github.com/aleclarson/mixpa",
+        "✔︎ Installed node_modules of 1 package",
+        "✔︎ Installed node_modules of 1 package",
+        "+ Linked gist:mixpa to gist/mixpa",
+        "✔︎ Local packages are linked!",
+      ]
+    `)
+  },
+  30 * 1e3
+)
