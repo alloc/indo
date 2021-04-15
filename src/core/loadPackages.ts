b83d07c8651a6f55da5e29ec5b351aa12c686dfa
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-14 21:16:22 -0400

    feat(loadPackages): better support for cloned repos and nested configs
    - When a cloned repo matches a vendor glob, it should still be crawled by loadPackages
    - When a cloned repo contains an indo config at its root, loadPackages is recursive

--- src/core/loadPackages.ts
+++ src/core/loadPackages.ts
@@ -1,17 +1,16 @@
 import globRegex from 'glob-regex'
 import { join } from 'path'
 import { crawl } from 'recrawl-sync'
-import { RootConfig } from './config'
+import { dotIndoId, loadConfig, RootConfig } from './config'
 import { fs } from './fs'
 import { GitIgnore } from './gitignore'
 import { loadPackage, PackageMap } from './Package'
 
-export function loadPackages(cfg: RootConfig) {
-  const packages: PackageMap = {}
+export function loadPackages(cfg: RootConfig, packages: PackageMap = {}) {
   const addPackage = (pkgPath: string) => {
     const pkg = loadPackage(join(cfg.root, pkgPath))!
     if (pkg.name && pkg.version) {
-      packages[pkg.name] = pkg
+      packages[pkg.name] ??= pkg
     }
   }
 
@@ -19,16 +18,36 @@ export function loadPackages(cfg: RootConfig) {
   findPackages(cfg.root, cfg.ignore).forEach(addPackage)
 
   const vendorRE = globRegex(cfg.vendor)
+  const nestedConfigs: RootConfig[] = []
 
   // Find packages in nested repostories.
   Object.keys(cfg.repos).forEach(repoDir => {
-    findPackages(join(cfg.root, repoDir), cfg.ignore).forEach(pkgPath => {
+    const absRepoDir = join(cfg.root, repoDir)
+
+    let ignoreRE = vendorRE
+    if (vendorRE.test(repoDir)) {
+      // Linked repos are skipped.
+      if (fs.isLink(absRepoDir)) return
+      // Nested configs are recursively handled.
+      const cfg = loadConfig(join(absRepoDir, dotIndoId))
+      if (cfg) {
+        return nestedConfigs.push(cfg)
+      }
+      // Cloned repos are crawled even if considered a vendor.
+      ignoreRE = /^$/
+    }
+
+    findPackages(absRepoDir, cfg.ignore).forEach(pkgPath => {
       pkgPath = join(repoDir, pkgPath)
-      if (!vendorRE.test(pkgPath)) {
+      if (!ignoreRE.test(pkgPath)) {
         addPackage(pkgPath)
       }
     })
   })
+
+  // Nested configs don't inherit our `ignore` array.
+  // Duplicate package names are skipped.
+  nestedConfigs.forEach(cfg => loadPackages(cfg, packages))
 
   return packages
 }
