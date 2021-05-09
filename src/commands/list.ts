c00725675b21ff083be82a4ce6e983dcaa95bc14
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-05-09 16:31:38 -0400

    feat: `indo ls` now prints unused packages last
    Closes #3

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -2,6 +2,7 @@ import os from 'os'
 import slurm from 'slurm'
 import { RootConfig } from '../core/config'
 import { cwdRelative, cyan, gray, log } from '../core/helpers'
+import { linkPackages } from '../core/linkPackages'
 import { loadAllPackages } from '../core/loadAllPackages'
 import { loadPackage, Package, toPackagePath } from '../core/Package'
 import { registry } from '../core/registry'
@@ -21,17 +22,42 @@ const loadGlobalPackages = () =>
 export default (cfg: RootConfig) => {
   const args = slurm({ g: true })
 
-  const packages = args.g
-    ? loadGlobalPackages()
-    : Object.values(loadAllPackages(cfg))
+  let packages: Package[]
+  if (args.g) {
+    packages = loadGlobalPackages()
+  } else {
+    const localPackages = loadAllPackages(cfg)
+    linkPackages(cfg, localPackages, { force: true, dryRun: true })
+    packages = Object.values(localPackages)
+  }
 
   packages.sort((a, b) => (a.name < b.name ? -1 : 1))
+
+  const unusedPackages: Package[] = []
+  if (!args.g) {
+    packages = packages.filter(pkg => {
+      if (!pkg.localDependents.size) {
+        unusedPackages.push(pkg)
+        return false
+      }
+      return true
+    })
+  }
 
   for (const pkg of packages) {
     log(
       gray('-'),
       pkg.name,
       pkg.version ? cyan(pkg.version) : '',
+      gray(args.g ? pkg.root.replace(os.homedir(), '~') : cwdRelative(pkg.root))
+    )
+  }
+
+  for (const pkg of unusedPackages) {
+    log(
+      gray('-'),
+      gray(pkg.name),
+      pkg.version ? gray(pkg.version) : '',
       gray(args.g ? pkg.root.replace(os.homedir(), '~') : cwdRelative(pkg.root))
     )
   }
