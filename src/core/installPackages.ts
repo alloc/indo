4b40fce7f23cb5d21698adfc5df79d50b59155f0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-05-12 11:22:32 -0400

    chore: remove `time` call in `installPackages`
    The call is not accurate because of how promises work.

--- src/core/installPackages.ts
+++ src/core/installPackages.ts
@@ -9,46 +9,47 @@ import { Package } from './Package'
 export async function installPackages(packages: Package[], force?: boolean) {
   const installed: Package[] = []
 
-  if (packages.length)
-    await time('install dependencies', async () => {
-      const installer = new AsyncTaskGroup(cpuCount, install)
-      await installer.concat(packages)
+  if (packages.length) {
+    const installer = new AsyncTaskGroup(cpuCount, install)
+    await installer.concat(packages)
 
-      async function install(pkg: Package) {
-        const deps = { ...pkg.dependencies, ...pkg.devDependencies }
-        if (!Object.keys(deps).length) return
+    async function install(pkg: Package) {
+      const deps = { ...pkg.dependencies, ...pkg.devDependencies }
+      if (!Object.keys(deps).length) return
 
-        const nodeModulesPath = join(pkg.root, 'node_modules')
-        if (force || !fs.isDir(nodeModulesPath)) {
-          const cpu = await requestCPU()
-          const task = startTask(
-            `Installing ${cyan(cwdRelative(pkg.root))} node_modules`
-          )
-          const logs: string[] = []
-          const npm = pkg.manager
-          try {
-            await npm.install((err, log) => {
+      const nodeModulesPath = join(pkg.root, 'node_modules')
+      if (force || !fs.isDir(nodeModulesPath)) {
+        const cpu = await requestCPU()
+        const task = startTask(
+          `Installing ${cyan(cwdRelative(pkg.root))} node_modules`
+        )
+        const logs: string[] = []
+        const npm = pkg.manager
+        try {
+          await time(`${npm.name} install`, () =>
+            npm.install((err, log) => {
               logs.push(err || log)
             })
+          )
 
-            installed.push(pkg)
-            task.finish()
-            log.debug('install completed:', yellow(cwdRelative(pkg.root)))
-            log.events.emit('install', pkg)
-          } catch (e) {
-            task.finish()
-            log.error('Installation failed:', yellow(cwdRelative(pkg.root)))
-            if (isTest) {
-              throw e
-            }
-            if (logs.length) log.error(logs.join('\n').trim())
-            else log.error(e.message)
-          } finally {
-            cpu.release()
+          installed.push(pkg)
+          task.finish()
+          log.debug('install completed:', yellow(cwdRelative(pkg.root)))
+          log.events.emit('install', pkg)
+        } catch (e) {
+          task.finish()
+          log.error('Installation failed:', yellow(cwdRelative(pkg.root)))
+          if (isTest) {
+            throw e
           }
+          if (logs.length) log.error(logs.join('\n').trim())
+          else log.error(e.message)
+        } finally {
+          cpu.release()
         }
       }
-    })
+    }
+  }
 
   return installed
 }
