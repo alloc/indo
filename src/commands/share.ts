e5ef9644e8e4d13766075096bbbc487a8aaa4aa9
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-12-06 16:14:05 -0500

    feat(share): accept relative path arguments
    To share a non-vendor git repository, pass the relative path to its root directory (where .git resides).

--- src/commands/share.ts
+++ src/commands/share.ts
@@ -1,6 +1,9 @@
-import { join, relative } from 'path'
+import { warn } from 'misty'
+import { join, relative, resolve } from 'path'
 import prompt, { Choice } from 'prompts'
+import slurm from 'slurm'
 import { RootConfig, saveConfig } from '../core/config'
+import { fs } from '../core/fs'
 import { git } from '../core/git'
 import { fatal, gray, log, yellow } from '../core/helpers'
 import { loadVendors } from '../core/loadVendors'
@@ -14,25 +17,53 @@ type RepoMap = {
 }
 
 export default async (cfg: RootConfig) => {
+  const gitRoots: string[] = []
+  const args = slurm()
+
+  const isExplicit = args.length > 0
+  if (isExplicit) {
+    for (const name of args) {
+      const gitRoot = resolve(name)
+      if (fs.exists(join(gitRoot, '.git'))) {
+        gitRoots.push(gitRoot)
+      } else {
+        warn(`"${name}" has no .git folder`)
+      }
+    }
+  } else {
+    const packages = loadVendors(cfg)
+    for (const pkg of Object.values(packages)) {
+      const gitRoot = git.findRoot(pkg.root)
+      if (gitRoot && gitRoot !== cfg.root) {
+        gitRoots.push(gitRoot)
+      }
+    }
+  }
+
+  if (!gitRoots.length) {
+    if (isExplicit) {
+      return log('Nothing to do.')
+    }
+    fatal('No vendor packages were found.')
+  }
+
   const repos: RepoMap = {}
   const choices: Choice[] = []
 
-  const packages = loadVendors(cfg)
-  for (const pkg of Object.values(packages)) {
-    const gitRoot = git.findRoot(pkg.root)
-    if (!gitRoot || gitRoot === cfg.root) {
+  for (const gitRoot of gitRoots) {
+    if (repos[gitRoot]) {
       continue
     }
-    if (!repos[gitRoot]) {
-      if (cfg.repos[gitRoot]) {
-        log('In tracked repo: %O', gitRoot)
-        continue
-      }
-      const root = relative(cfg.root, gitRoot)
-      if (!repos[root]) {
-        const url = git.getRemoteUrl(gitRoot)
-        const head = git.getActiveBranch(gitRoot)
-        repos[root] = { root, url, head }
+    if (cfg.repos[gitRoot]) {
+      log('In tracked repo: %O', gitRoot)
+      continue
+    }
+    const root = relative(cfg.root, gitRoot)
+    if (!repos[root]) {
+      const url = git.getRemoteUrl(gitRoot)
+      const head = git.getActiveBranch(gitRoot)
+      repos[root] = { root, url, head }
+      if (!isExplicit)
         choices.push({
           title: root,
           value: root,
@@ -40,21 +71,18 @@ export default async (cfg: RootConfig) => {
             gray(' - ' + url.replace(/^https:\/\//, '')) +
             (head == 'master' ? '' : yellow(' ' + head)),
         })
-      }
     }
   }
 
-  if (!choices.length) {
-    return fatal('No vendor packages were found.')
-  }
-
   type Answer = { selected: string[] }
-  const { selected }: Answer = await prompt({
-    name: 'selected',
-    type: 'autocompleteMultiselect',
-    message: 'Choose which repos to share',
-    choices,
-  })
+  const { selected }: Answer = isExplicit
+    ? { selected: Object.keys(repos) }
+    : await prompt({
+        name: 'selected',
+        type: 'autocompleteMultiselect',
+        message: 'Choose which repos to share',
+        choices,
+      })
 
   for (const root of selected) {
     const remotes = git.getRemotes(join(cfg.root, root))
